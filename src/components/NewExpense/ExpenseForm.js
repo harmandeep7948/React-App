819f9a7f0d343cde1b2c7cf6b05978f3e5149431
harmandeep.singh <harmandeep.singh@webners.com>
2022-03-29 17:52:34 +0530

    changes

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -21,11 +21,18 @@ const ExpenseForm = () => {
 
   const submitHandler = (event) => {
     event.preventDefault();
+    setEnteredTitle('');
+    console.log(enteredTitle);
     const expenseData = {
       title: enteredTitle,
       amount: enteredAmount,
       date: new Date(enteredDate),
     };
+    
+
+    
+    // setEnteredAmount('');
+    // setEnteredDate('');
     console.log(expenseData);
   };
 
@@ -34,16 +41,17 @@ const ExpenseForm = () => {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input type="text" value={enteredTitle} onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" onChange={amountChangeHandler} />
+          <input type="number" value={enteredAmount} min="0.01" onChange={amountChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
           <input
             type="date"
+            value={enteredDate}
             min="2019-01-01"
             max="2022-12-31"
             onChange={dateChangeHandler}
