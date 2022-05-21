86629549ba388a8fa7187c22778955c3a449f6b7
harmandeep7948 <harmandeepsingh7948@gmail.com>
2022-05-21 13:17:14 +0530

    test changes

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,22 @@
+import React, { useState } from 'react';
+
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
+  console.log("Expense item is called");
+  const [title, setTitle] = useState(props.title);
   const eventHandler = () => {
-    console.log("Clicked !!");
+    setTitle("Updated!");
+    console.log("after settile");
+    console.log(title);
   }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={eventHandler}>Click Me!</button>
